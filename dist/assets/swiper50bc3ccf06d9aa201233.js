0da2b8970169e7efab253ddb09c9ad0f6375b20c
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2023-10-21 00:24:48 +0300

    styles for mobile

---
+++ dist/assets/swiper50bc3ccf06d9aa201233.js
@@ -0,0 +1,1 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:6,spaceBetween:30,edgeSwipeThreshold:30,autoplay:{delay:3e3}});
--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -1,10 +1,17 @@
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 3,
   spaceBetween: 30,
   edgeSwipeThreshold: 30,
   autoplay: {
     delay: 3000,
   },
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 30
+    },
+  }
 });
