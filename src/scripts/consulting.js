2fc361985c1de4ff5cd59d407230e04533124ffe
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2023-10-21 20:04:58 +0300

    homepage mobile styles 3.0

--- src/scripts/consulting.js
+++ src/scripts/consulting.js
@@ -6,8 +6,10 @@ links.forEach(link => highlightActivePage(link));
 
 const mobNavBtn = document.querySelector(".mobile-menu-btn");
 const mobLinks = document.querySelectorAll(".mobile-navigation__link");
+const closeBtn = document.querySelector(".mobile-navigation__close-btn");
 
 mobNavBtn.addEventListener("click", showMobileNav);
+closeBtn.addEventListener('click', hideMobileNav);
 
 mobLinks.forEach(function (link) {
   link.addEventListener("click", hideMobileNav);
