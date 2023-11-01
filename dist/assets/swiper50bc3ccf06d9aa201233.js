9c53aba7f9122baf0dffef18ff0798836711dabc
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2023-11-01 15:45:55 +0200

    solutions

--- dist/assets/swiper50bc3ccf06d9aa201233.js
+++
@@ -1,1 +0,0 @@
-const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:6,spaceBetween:30,edgeSwipeThreshold:30,autoplay:{delay:3e3}});
---
+++ dist/assets/swiper7b961d24bcd66e0ceffe.js
@@ -0,0 +1,1 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:3,spaceBetween:30,edgeSwipeThreshold:30,autoplay:{delay:3e3},breakpoints:{992:{slidesPerView:6,spaceBetween:30}}});
