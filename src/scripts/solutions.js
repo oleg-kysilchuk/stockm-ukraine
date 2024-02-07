32bec15d64cace12357deaef076073d6a27e3807
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2024-02-07 20:02:22 +0200

    jgh8e08s

--- src/scripts/solutions.js
+++ src/scripts/solutions.js
@@ -1,17 +1,32 @@
 import "../pages/solutions.html";
 import "../styles/solutions.scss";
-import { highlightActivePage, links, showMobileNav, hideMobileNav } from '../modules/util';
+import baguettebox from "../modules/baguetteBox.min.js";
+import {
+  highlightActivePage,
+  links,
+  showMobileNav,
+  hideMobileNav,
+} from "../modules/util";
 
-links.forEach(link => highlightActivePage(link));
+links.forEach((link) => highlightActivePage(link));
 
 const mobNavBtn = document.querySelector(".mobile-menu-btn");
 const mobLinks = document.querySelectorAll(".mobile-navigation__link");
 const closeBtn = document.querySelector(".mobile-navigation__close-btn");
 
 mobNavBtn.addEventListener("click", showMobileNav);
-closeBtn.addEventListener('click', hideMobileNav);
+closeBtn.addEventListener("click", hideMobileNav);
 
 mobLinks.forEach(function (link) {
   link.addEventListener("click", hideMobileNav);
 });
 
+baguettebox.run(".presentation", {
+  captions: false,
+  ignoreClass: 'gallery-button'
+});
+
+const galleryLinks = [...document.querySelectorAll(".presentation a")];
+const changedHref = galleryLinks.map((a) => {
+  a.href = a.children[0].src;
+});
