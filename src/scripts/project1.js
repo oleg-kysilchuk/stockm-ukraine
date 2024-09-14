42142e393ebeb1da73e882a0c4ce715d63bd5b71
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2024-09-14 22:07:43 +0300

    w45rye

--- src/scripts/project1.js
+++ src/scripts/project1.js
@@ -1,2 +1,9 @@
 import "../pages/project1.html";
 import "../styles/project1.scss";
+
+const problemTabs = document.querySelectorAll(".project-problems__tab-question");
+problemTabs.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("active");
+    });
+});
