4691b79177ef7b4dd0a0a5827fd297ee6ba0e9b7
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2025-01-23 20:15:49 +0200

    4ggtrh

--- src/scripts/project-kpi.js
+++ src/scripts/project-kpi.js
@@ -1,5 +1,5 @@
 import "../pages/project-kpi.html";
-import "../styles/project-single.scss";
+import "../styles/project-single-two.scss";
 
 const problemTabs = document.querySelectorAll(".project-problems__tab-question");
 problemTabs.forEach((item) => {
