7057d189786805231ea86a2ccc70a3103d01ec04
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2025-03-05 17:30:50 +0200

    t4w54y54

--- src/scripts/project-shchodnia.js
+++ src/scripts/project-shchodnia.js
@@ -1,5 +1,8 @@
 import "../pages/project-shchodnia.html";
-import "../styles/project-single.scss";
+import "../styles/project-single-two.scss";
+import '../modules/owl-slider';
+import '../styles/owl/owl.carousel.min.css'
+import '../styles/owl/owl.theme.default.min.css'
 
 const problemTabs = document.querySelectorAll(".project-problems__tab-question");
 problemTabs.forEach((item) => {
